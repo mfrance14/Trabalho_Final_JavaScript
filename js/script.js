32f5bd2a05bf45386c51e26dc71b0c9ccf5b0b5f
Loren <loren_alonso21@hotmail.com>
2021-02-18 14:59:27 -0300

    Corrigindo js

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ function categoria(){
         let itemlateral = document.createElement("a");
         let item = document.createElement("li");
         itemlateral.innerHTML = listacategoria[j];
+        itemlateral.href = `#${j}`;
         item.appendChild(itemlateral);
         menulateral.appendChild(item);
         
@@ -16,58 +17,58 @@ function categoria(){
 categoria();
 
 function cards(){
-    let categoriaAnterior = 0;
     let listacards = [];
     
     let cardslivros = document.querySelector("main");
     for(let i = 0; i<livros.length;i++){
-        listacards.push(livros[i]); //percorre o arrray e coloca na lista
+        listacards.push(livros[i]); 
     }
     
     for(let w = 0; w<categorias.length;w++){
         let divcategoria = document.createElement("div");
-        divcategoria.id = `${categorias[w]}`;
-        let divrow = document.createElement("div");
-        divrow.className = "row";
-        divcategoria.appendChild(divrow);
-        cardslivros.appendChild(divcategoria);
-    }
-    for (let j = 0; j < listacards.length; j++) {
-        let seleciona = `${listacards[j].categoria} row`;
-       let categoriaAtual = document.querySelector(seleciona);
+        divcategoria.id = `${w}`;
+        divcategoria.className = "row";
 
-       
-        let divcolumn2 = document.createElement("div");
-        divcolumn2.className("column2");
-        let divcard = document.createElement("div");
-        divcard.className("card");
-        let imagem = document.createElement("img");
-        imagem.className("card-img");
-        imagem.src = listacards[j].imagem;
-        imagem.alt = "Imagem do card";
-        let divcardbody = document.createElement("div");
-        divcardbody.className("card-body");
-        let divcardtitle = document.createElement("h5");
-        divcardtitle.className("card-title");
-        let divcardtext = document.createElement("p");
-        divcardtext.className("card-text");
-        let divbutton = document.createElement("a");
-        divbutton.className("btn btn-primary");
-        divbutton.innerHTML = "Comprar";
-
-        divcardbody.appendChild(divcardtitle);
-        divcardbody.appendChild(divcardtext);
-        divcardbody.appendChild(divbutton);
-
-        divcard.appendChild(imagem);
-        divcard.appendChild(divcardbody);
-
-        divcolumn2.appendChild(divcard);
+        for (let j = 0; j < listacards.length; j++) {
+            if (w == listacards[j].categoria) {
+                let divcolumn2 = document.createElement("div");
+                divcolumn2.className = "column2";
+                let divcard = document.createElement("div");
+                divcard.className = "card";
+                let imagem = document.createElement("img");
+                imagem.className = "card-img";
+                imagem.src = listacards[j].imagem;
+                imagem.alt = "Imagem do card";
+                let divcardbody = document.createElement("div");
+                divcardbody.className = "card-body";
+                let divcardtitle = document.createElement("h5");
+                divcardtitle.className = "card-title";
+                divcardtitle.innerHTML = ` ${listacards[j].nome}`;
+                let divcardtext = document.createElement("p");
+                divcardtext.className = "card-text";
+                divcardtext.innerHTML = `R$ ${listacards[j].preco}`;
+                let divbutton = document.createElement("a");
+                divbutton.className = "btn btn-primary";
+                divbutton.innerHTML = "Comprar";
         
-        categoriaAtual.appendChild(divcolumn2);
+                divcardbody.appendChild(divcardtitle);
+                divcardbody.appendChild(divcardtext);
+                divcardbody.appendChild(divbutton);
+                divcard.appendChild(imagem);
+                divcard.appendChild(divcardbody);
+                divcolumn2.appendChild(divcard);
+                divcategoria.appendChild(divcolumn2);  
+                cardslivros.appendChild(divcategoria);
+            }
+        }
+    }
 
-        categoriaAnterior = listacards[j].categoria;
-      
-    } 
+
+
 }
+
+
+
+
+
 cards();
