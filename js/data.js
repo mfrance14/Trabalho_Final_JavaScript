1bd3fa3ce22af8239b533277ceb5f85f83a58121
Loren <loren_alonso21@hotmail.com>
2021-02-19 18:13:09 -0300

    Adicionando livros e precos

--- js/data.js
+++ js/data.js
@@ -7,79 +7,79 @@ let categorias = [ // array para filtrar as categorias na barra lateral
 ]; 
 
 let livros = [
-    {"nome": "Eclipse",
+    {"nome": "Mil beijos de um garoto",
     "categoria": 0,
-    "preco": 30.55,
-    "imagem": "imagens/eclipse.jpg"
+    "preco": 47.90,
+    "imagem": "imagens/R1.jpg"
     },
-    {"nome": "Amanhecer",
+    {"nome": "Todas as suas IMperfeições",
     "categoria": 0,
-    "preco": 30.00,
-    "imagem": "imagens/eclipse.jpg"
+    "preco": 29.90,
+    "imagem": "imagens/R2.jpg"
     },
-    {"nome": "Eclipse",
+    {"nome": "Tudo e Todas as coisas",
     "categoria": 0,
-    "preco": 30.00,
-    "imagem": "imagens/eclipse.jpg"
+    "preco": 25.11,
+    "imagem": "imagens/R3.jpg"
     },
-    {"nome": "Eclipse",
+    {"nome": "Amigo Imaginário",
     "categoria": 1,
-    "preco": 30.00,
-    "imagem": "imagens/eclipse.jpg"
+    "preco": 45.90,
+    "imagem": "imagens/T1.jpg"
     },
-    {"nome": "Eclipse",
+    {"nome": "O Exorcista",
     "categoria": 1,
-    "preco": 30.00,
-    "imagem": "imagens/eclipse.jpg"
+    "preco": 39.90,
+    "imagem": "imagens/T2.jpg"
     },
-    {"nome": "Eclipse",
+    {"nome": "A assombração da casa da colina",
     "categoria": 1,
-    "preco": 30.00,
-    "imagem": "imagens/eclipse.jpg"
+    "preco": 34.90,
+    "imagem": "imagens/T3.jpg"
     },
-    {"nome": "Eclipse",
+    {"nome": "Jogos Vorazes",
     "categoria": 2,
-    "preco": 30.00,
-    "imagem": "imagens/eclipse.jpg"
+    "preco": 37.89,
+    "imagem": "imagens/F1.jpg"
     },
-    {"nome": "Eclipse",
+    {"nome": "Corte de Espinhos e Rosas",
     "categoria": 2,
-    "preco": 30.00,
-    "imagem": "imagens/eclipse.jpg"
+    "preco": 34.87,
+    "imagem": "imagens/F2.jpg"
     },
-    {"nome": "Eclipse",
+    {"nome": "O Hobbit",
     "categoria": 2,
-    "preco": 30.00,
-    "imagem": "imagens/eclipse.jpg"
+    "preco": 31.90,
+    "imagem": "imagens/F3.jpg"
     },
-    {"nome": "Eclipse",
+    {"nome": "O Homem de Giz",
     "categoria": 3,
-    "preco": 30.00,
-    "imagem": "imagens/eclipse.jpg"
+    "preco": 36.90,
+    "imagem": "imagens/S1.jpg"
     },
-    {"nome": "Eclipse",
+    {"nome": "A mulher na Janela",
     "categoria": 3,
-    "preco": 30.00,
-    "imagem": "imagens/eclipse.jpg"
+    "preco": 39.90,
+    "imagem": "imagens/S4.jpg"
     },
-    {"nome": "Eclipse",
+    {"nome": "Malorie",
     "categoria": 3,
-    "preco": 30.00,
-    "imagem": "imagens/eclipse.jpg"
+    "preco": 29.90,
+    "imagem": "imagens/S3.jpg"
     },
-    {"nome": "Eclipse",
+    {"nome": "O poder do Agora",
     "categoria": 4,
-    "preco": 30.00,
-    "imagem": "imagens/eclipse.jpg"
+    "preco": 27.90,
+    "imagem": "imagens/A1.jpg"
     },
-    {"nome": "Eclipse",
+    {"nome": "Mindset",
     "categoria": 4,
-    "preco": 30.00,
-    "imagem": "imagens/eclipse.jpg"
+    "preco": 39.87,
+    "imagem": "imagens/A2.jpg"
     },
-    {"nome": "Eclipse",
+    {"nome": "A sutil arte de ligar o f*da-se",
     "categoria": 4,
-    "preco": 30.00,
-    "imagem": "imagens/eclipse.jpg"
+    "preco": 27.90,
+    "imagem": "imagens/A3.jpg"
     }
 ]
